22d7f138eca4c106b44a2063fe39bd6757d30441
Ashraful Haque <vselfnet@gmail.com>
2023-04-08 13:53:22 +0200

    Added React Hot Toast

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Header></Header>
             <hr />
             <Outlet></Outlet>
+            <Toaster></Toaster>
         </div>
     );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,18 +3,27 @@ import { useLoaderData } from 'react-router-dom';
 import Tshirt from './Tshirt';
 import Cart from './Cart';
 import './Layout.css';
+import { toast } from 'react-hot-toast';
 
 const Layout = () => {
     const tshirts = useLoaderData();
     
     const [cart, setCart] = useState([]);
     const handeledButton = tshirt => {
-        const newCart = [...cart, tshirt];
-        setCart(newCart);
+        const exist = cart.find(findShirt => findShirt._id === tshirt._id)
+        if(exist){
+            toast('Already added')
+        }
+        else{
+            // part-1
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
     }
 
     const removeCartItem = id => {
-        const remaining = cart.filter(remove => remove._id !== id)
+        // part-1
+        const remaining = cart.filter(remove => remove._id !== id);
         setCart(remaining);
 
     }
